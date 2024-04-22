8b1754264f60f4f69207aa45282a452383d391ed
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-04-21 21:09:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,41 +3,45 @@ from stmol import showmol
 import py3Dmol
 
 def read_sdf_from_string(content):
-    """ Parsea el contenido de un archivo SDF desde un string. """
     lines = content.split('\n')
     xyz_data = ''
     reading_molecule = False
 
     for line in lines:
         if line.startswith('$$$$'):
-            reading_molecule = False
+            reading_molecule = False  # Finalizar la lectura al encontrar el final del bloque de molécula
+        elif line.strip().endswith('V2000'):
+            reading_molecule = True  # Iniciar la lectura después de encontrar 'V2000'
+            continue  # Evitar incluir la línea 'V2000' en el procesamiento
+
         if reading_molecule:
-            parts = line.split()
-            if len(parts) >= 4 and parts[3].isalpha():
+            parts = line.strip().split()
+            if len(parts) >= 4 and parts[3].isalpha():  # Verifica si la línea contiene coordenadas y símbolos
                 try:
-                    x, y, z = float(parts[0]), float(parts[1]), float(parts[2])
+                    x, y, z = map(float, parts[:3])
                     atom_type = parts[3]
-                    xyz_data += f'{atom_type} {x:.3f} {y:.3f} {z:.3f}\n'
+                    xyz_line = f'{atom_type} {x:.3f} {y:.3f} {z:.3f}\n'
+                    xyz_data += xyz_line
                 except ValueError:
-                    continue
-        if line.strip().endswith('V2000'):
-            reading_molecule = True
-        st.write(xyz_data)
+                    continue  # Manejo de error por si alguna conversión falla
+
     return xyz_data
 
 def plot_molecule_with_stmol(xyz_data):
-    """ Muestra la molécula utilizando py3Dmol. """
+    if not xyz_data:
+        st.error("No molecule data found. Please check the SDF file.")
+        return
+
     xyzview = py3Dmol.view(width=800, height=400)
-    xyzview.addModel(xyz_data, 'sdf')
+    xyzview.addModel(xyz_data, 'xyz')
     xyzview.setStyle({'sphere': {}})
-    xyzview.setBackgroundColor('green')
+    xyzview.setBackgroundColor('white')
     xyzview.zoomTo()
     showmol(xyzview, height=500, width=800)
 
 # Streamlit App
 st.title('Visualización Molecular 3D')
 
-# Cargar archivo SDF
 uploaded_file = st.file_uploader("Upload your SDF file", type=["sdf"])
 if uploaded_file is not None:
     file_content = uploaded_file.getvalue().decode("utf-8")
