b83364b5a994f7e722dfcb479849176f98013217
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-04-27 12:05:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,10 @@ angle_y = st.sidebar.slider('Rotation angle around Y-axis (degrees)', 0, 360, 0)
 angle_z = st.sidebar.slider('Rotation angle around Z-axis (degrees)', 0, 360, 0)
 
 rotated_molecule = apply_rotation(molecule, (angle_x, angle_y, angle_z))
-st.sidebar.number_input('x direction',value=1)
-direction_vector = np.array([1, 0, 0])
+x_d=st.sidebar.number_input('x direction',value=1)
+y_d=st.sidebar.number_input('y direction',value=0)
+z_d=st.sidebar.number_input('z direction',value=0)
+direction_vector = np.array([x_d, y_d, z_d])
 max_displacement,atom_i,atom_j = calculate_contact(rotated_molecule, direction_vector)
 displacement_vector = np.array([max_displacement, 0, 0])
 transformed_coords = rotated_molecule.coordinates + displacement_vector
