e4215cfe81349b1e43fe652f670324ed8ab40451
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-04-27 11:56:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -151,11 +151,11 @@ st.markdown("## Displacement: "+"{:.2f}".format(max_displacement) + " Å")
 plot_molecule_with_stmol(original_xyz,transformed_xyz)
 
 
-coordA =rotated_molecule.coordinates[atom_i] 
-coordA_=coordA - displacement_vector
-coordB =rotated_molecule.coordinates[atom_j]
-st.write(np.linalg.norm(coordB-coordA_ ))
-st.write(rotated_molecule.get_radius(atom_i) + rotated_molecule.get_radius(atom_j))
+#coordA =rotated_molecule.coordinates[atom_i] 
+#coordA_=coordA - displacement_vector
+#coordB =rotated_molecule.coordinates[atom_j]
+#st.write(np.linalg.norm(coordB-coordA_ ))
+#st.write(rotated_molecule.get_radius(atom_i) + rotated_molecule.get_radius(atom_j))
 
 st.write(coordA)
 st.write(coordA_)
