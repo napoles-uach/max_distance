e122b3f17fb9eeebeea9adca3f7eb54dd3982103
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-07-15 11:06:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ st.download_button(
     mime='text/plain',
 )
 
-st.markdown("# Ask a question about the paper.")
+st.sidebar.markdown("# Ask a question about the paper.")
 
 process_paper(api_key=st.secrets["gpt_key"])
 
